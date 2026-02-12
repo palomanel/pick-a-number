21b2c13186f549483dd2e882a5e42948f78fd023
Paulo Albuquerque <paulo.albuquerque@gmail.com>
2026-02-12 12:31:44 +0000

    feat: X-ray

--- src/diagram/jamstack-architecture.py
+++ src/diagram/jamstack-architecture.py
@@ -9,6 +9,7 @@ from diagrams.aws.network import APIGateway
 from diagrams.aws.compute import Lambda
 from diagrams.aws.database import Dynamodb
 from diagrams.aws.management import CloudwatchLogs
+from diagrams.aws.devtools import XRay
 
 graph_attr = {
     "layout": "dot",
@@ -33,7 +34,7 @@ with Diagram(
         with Cluster("API Layer"):
             api_gateway = APIGateway("API Gateway")
             submit_number_lambda = Lambda("SubmitNumber\nLambda function")
-            daily_stats_lambda = Lambda("DailyStats\nLambda function")
+            stats_lambda = Lambda("Stats\nLambda function")
 
         with Cluster("Data Layer"):
             dynamodb = Dynamodb("DynamoDB\nTable")
@@ -41,6 +42,7 @@ with Diagram(
         with Cluster("Management\nLayer"):
             logs = S3("Logs\nS3 Bucket")
             cloudwatch = CloudwatchLogs("CloudWatch\nLogs")
+            xray = XRay("X-Ray\nTracing")
 
     user >> cloudfront
     cloudfront >> Edge(label="/", minlen="2") >> s3
@@ -54,8 +56,8 @@ with Diagram(
     )
     (
         api_gateway
-        >> Edge(label="/api/daily-stats", minlen="2")
-        >> daily_stats_lambda
+        >> Edge(label="/api/stats", minlen="2")
+        >> stats_lambda
         >> Edge(minlen="2")
         >> dynamodb
     )
@@ -64,4 +66,7 @@ with Diagram(
     s3 >> Edge(style="dashed") >> logs
     api_gateway >> Edge(style="dashed") >> cloudwatch
     submit_number_lambda >> Edge(style="dashed") >> cloudwatch
-    daily_stats_lambda >> Edge(style="dashed") >> cloudwatch
+    stats_lambda >> Edge(style="dashed") >> cloudwatch
+    api_gateway >> Edge(style="dashed") >> xray
+    submit_number_lambda >> Edge(style="dashed") >> xray
+    stats_lambda >> Edge(style="dashed") >> xray
